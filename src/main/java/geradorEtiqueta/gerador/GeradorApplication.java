cf265c7dd2208935eb08299039e76f14a010b6a9
yuriglecios <glleciosyuri@gmail.com>
2024-05-12 11:21:56 -0300

    Criação do projeto base

---
+++ src/main/java/geradorEtiqueta/gerador/GeradorApplication.java
@@ -0,0 +1,13 @@
+package geradorEtiqueta.gerador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorApplication.class, args);
+	}
+
+}
